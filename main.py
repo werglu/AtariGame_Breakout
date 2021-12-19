a7dd1088a9578d4a9becfd221f7ef210c935ec5a
orion4017 <adk4017@gmail.com>
2021-12-19 10:51:47 +0100

    Modify main for readability

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from ale_py import ALEInterface
 from ale_py.roms import Breakout
 import gym
+import time
 
 ale = ALEInterface()
 ale.loadROM(Breakout)
 
-env = gym.make('Breakout-v0')
+# env = gym.make('Breakout-v0')
+# env = gym.make('BreakoutDeterministic-v4')
+env = gym.make('BreakoutNoFrameskip-v4')
 env.reset()
-for _ in range(100000):
+for _ in range(10000):
     env.render()
+    time.sleep(0.01) # good for readability
     env.step(env.action_space.sample()) # take a random action
 env.close()
